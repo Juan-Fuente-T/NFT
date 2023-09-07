f1635754becd27b72d474d20638b3654dbb6cb63
Juan_Fuente_T <jfuentet@gmail.com>
2023-09-07 23:37:51 +0200

    07-09-23

--- NFT.sol
+++ NFT.sol
@@ -15,4 +15,27 @@ contract NFT is ERC721 {
         string nombre;
         string descripcion;
     }
-   
+    //hacemos un mapping para guardar los datos del nft en variables tipo datos
+    mapping(uint256 => DatosNFT) datos;
+    //el contructor del ERC721 necesita name y symbol
+    constructor() ERC721("Paisaje", "PSJ") {
+
+    }
+    //funcion para mintear los nft, evalua que el valor que envia msg.sender sea mayor que el precio del nft
+    function mint() public payable {
+        require(msg.value >= PRICE, "No has pagado el NFT");
+        //funcion que emite los tokens, toma por parametro a quien los envia y el numero id del token
+        _mint(msg.sender, totalSupply);
+        //se actualiza el numero de tokens minteados 
+        totalSupply++;
+    }
+    //funcion que pasa los metadatos al token y devuelve un string con los datos)sobreescribe la funcion del ERC721
+    function tokenURI(uint256 tokenId) public override view returns (string memory) {
+        /*
+        {
+            "name": "PAISAJE",
+            "description": "Un paisaje bonito",
+            "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
+        }
+        */
+        
